2d27e2230f89f9570b4ec15c8404e730851e2b71
Anjesh Bhattarai <bhattaraianjesh123@gmail.com>
2024-04-13 17:26:30 +0930

    "use client";
    import React from "react";
    import { usePathname } from "next/navigation";
    
    import styles from "./navbar.module.css";
    import { MdSearch } from "react-icons/md";
    const Navbar = () => {
      const pathname = usePathname();
    
      return (
        <div className={styles.container}>
          <div className={styles.title}>{pathname.split("/").pop}</div>
          <div className={styles.menu}>
            <div className={styles.search}>
              {" "}
              <MdSearch />
              <input type="text" placeholder="Search..." />
            </div>
          </div>
        </div>
      );
    };
    
    export default Navbar;

--- src/app/ui/dashboard/navbar/navbar.jsx
+++ src/app/ui/dashboard/navbar/navbar.jsx
@@ -1,15 +1,25 @@
-"use client"
+"use client";
 
-import React from 'react'
+import React from "react";
 import { usePathname } from "next/navigation";
 
-import styles from "./navbar.module.css"
+import styles from "./navbar.module.css";
+import { MdSearch } from "react-icons/md";
 const Navbar = () => {
-    const pathname = usePathname(); 
+  const pathname = usePathname();
 
   return (
-    <div className={styles.container}>{pathname.split("/").pop}</div>
-  )
-}
+    <div className={styles.container}>
+      <div className={styles.title}>{pathname.split("/").pop}</div>
+      <div className={styles.menu}>
+        <div className={styles.search}>
+          {" "}
+          <MdSearch />
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
